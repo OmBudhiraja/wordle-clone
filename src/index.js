f3815d071a8231b91465d5655fc868bd8eb8203b
Om Budhiraja <ombudhiraja144@gmail.com>
2022-06-13 17:57:55 +0530

    removing react-devtools from production

--- src/index.js
+++ src/index.js
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
+if (process.env.NODE_ENV === 'production') {
+  if (window && window.__REACT_DEVTOOLS_GLOBAL_HOOK__) {
+    window.__REACT_DEVTOOLS_GLOBAL_HOOK__.inject = function () {};
+  }
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
